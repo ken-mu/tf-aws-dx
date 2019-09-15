f79315dc0e39ce7c4740b4a9618e098304de195f
ken-mu <gas.010bl@gmail.com>
2019-09-15 15:34:07 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,24 @@ provider "aws" {
   secret_key = var.aws_access_secret
 }
 
+resource "aws_vpc" "main" {
+  cidr_block       = "10.1.0.0/16"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "private-1" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.1.0.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-1"
+  }
+}
+
 resource "aws_ec2_transit_gateway" "example" {
   amazon_side_asn = "64512"
   auto_accept_shared_attachments = "disable"
@@ -18,4 +36,20 @@ resource "aws_ec2_transit_gateway" "example" {
     Name = "example"  
   }
   vpn_ecmp_support = "disable"
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "main" {
+  transit_gateway_id = "${aws_ec2_transit_gateway.example.id}"
+  vpc_id             = "${aws_vpc.main.id}"
+
+  transit_gateway_default_route_table_association = "false"
+  transit_gateway_default_route_table_propagation = "false"
+
+  subnet_ids = [
+    "${aws_subnet.private-1.id}",
+  ]
+  
+  tags = {
+    Name = "main"
+  }
 }
