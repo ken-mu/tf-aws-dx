175bd460d1f8fd1e9e4c8a5ee43ad6f66a26ac9b
ken-mu <gas.010bl@gmail.com>
2019-09-15 11:46:55 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+variable "aws_access_key" {}
+variable "aws_access_secret" {}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_access_secret
+}
+
+data "aws_ec2_transit_gateway" "example" {
+  id = "example"
+}
